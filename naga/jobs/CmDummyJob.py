dd515e09048cacd439356083f92aba5049d6cf91
Venkatesh Tadinada <vtadinad@bmc.com>
2021-07-24 22:04:51 -0700

    moved jobs to jobs.py

--- naga/jobs/CmDummyJob.py
+++ naga/jobs/CmDummyJob.py
@@ -2,8 +2,8 @@ from naga.jobs.CmBaseJob import CmBaseJob
 
 
 class CmDummyJob(CmBaseJob):
-    def __init__(self,folder, jobName):
-        CmBaseJob.__init__(self,folder, jobName)
+    def __init__(self,folder, job_name):
+        CmBaseJob.__init__(self,folder, job_name)
     def getJson(self):
         jobJson =  CmBaseJob.getJson(self)
         jobJson["Type"] = "Job:Dummy" 
