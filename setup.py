7fa53176536a6aeaf1ddda79d5fc6411dfe3712a
Venkatesh Tadinada <vtadinad@bmc.com>
2021-07-24 18:37:05 -0700

    added setup info

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='naga',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python Workflows for Control-M',
+    author='Venkatesh Tadinada @ BMC Innovations Lab',
+    license='Apache 2.0',
+)
