aed59212f78ee0f1bd67a459d4379e0768cd7766
Venkatesh Tadinada <vtadinad@bmc.com>
2021-07-25 18:09:47 -0700

    created generic AI Jobs and BlobStorage

--- naga/jobs/application_integrator/generic_ai.py
+++ naga/jobs/application_integrator/generic_ai.py
@@ -7,13 +7,13 @@ class GenericAiJob(BaseJob):
     ):
         BaseJob.__init__(self, folder, job_name, description, host, run_as)
         self.connection_profile = connection_profile
-        self.aai_namei_paraaims = ai_name
+        self.ai_name = ai_name
         self.ai_params = kwargs
 
     def get_json(self):
         job_json = BaseJob.get_json(self)
         job_json["Type"] = "Job:ApplicationIntegrator:" + self.ai_name
         job_json["ConnectionProfile"] = self.connection_profile
-        for key, value in kwargs.items():
+        for key, value in self.ai_params.items():
             job_json[key] = value
         return job_json
