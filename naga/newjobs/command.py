9a275ad9c3e38e0bf65badb7de694ff1302a7d66
Venkatesh Tadinada <vtadinad@bmc.com>
2021-08-07 09:28:45 -0700

    working generation of job files

--- naga/newjobs/command.py
+++ naga/newjobs/command.py
@@ -2,23 +2,23 @@ from naga.jobs.base import BaseJob
 
 class CommandJob(BaseJob):
 	def __init__(self, folder, job_name, 
-				created_by,
 				command,
-				events_to_add,
+				pre_command = None,
+				post_command = None,
 				host=None, run_as=None, description=None):
 
 		BaseJob.__init__(self, folder, job_name, description=description, host=host, run_as=run_as)
-		self.created_by = created_by
 		self.command = command
-		self.events_to_add = events_to_add
+		self.pre_command = pre_command
+		self.post_command = post_command
 
 	def get_json(self):
 		job_json = BaseJob.get_json(self)
 		job_json['Type'] = 'Job:Command'
-		if self.created_by != None:
-			job_json['CreatedBy'] = self.created_by
 		if self.command != None:
 			job_json['Command'] = self.command
-		if self.events_to_add != None:
-			job_json['eventsToAdd'] = self.events_to_add
+		if self.pre_command != None:
+			job_json['PreCommand'] = self.pre_command
+		if self.post_command != None:
+			job_json['PostCommand'] = self.post_command
 		return job_json
