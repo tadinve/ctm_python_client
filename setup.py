8ea775857c20583573432d1cfc7c1c49195a80b4
Venkatesh Tadinada <vtadinad@bmc.com>
2021-08-23 09:06:14 -0700

    removed reference to graphviz

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='naga',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0',
     description='Python Workflows for Control-M',
     author='Venkatesh Tadinada @ BMC Innovations Lab',
     license='Apache 2.0',
-    install_requires=[
-          'graphviz>=0.10', 'requests>=2.23.0',
-    ],
+    install_requires=['requests>=2.23.0',],
 )
