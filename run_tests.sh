f313ae4a1b550273f48bdcd3e1937a5d163753bd
Venkatesh Tadinada <vtadinad@bmc.com>
2021-10-04 08:33:45 -0700

    made testing changes

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 pip uninstall ctm_python_client -y
 python -m pytest --cov=ctm_python_client
-coverage run -m --source=./ctm_python_client pytest
+coverage run -m --source=./ctm_python_client pytest -rA tests/*.py tests/tests_indiv_jobs/*.py   && coverage report -m
 pytest --nbmake  examples/python_notebooks/*.ipynb
+coverage run -m  --source=./ctm_python_client/core pytest -rA tests/test_core_flow.py tests/test_complete_flow.py && coverage report -m
